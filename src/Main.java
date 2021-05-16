dfb015b61a9a510e449a54339383910fa9ea3e29
Jeison Guerrero <jei_son1516@hotmail.com>
2021-05-16 00:22:44 -0400

    doctor class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        
+        //showMenu();
+
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 
 
