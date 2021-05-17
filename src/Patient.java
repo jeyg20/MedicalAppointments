313b8fb0c11ca28cd741139f739bd0ae842c3de0
Jeison Guerrero <jei_son1516@hotmail.com>
2021-05-17 11:13:07 -0400

    Patient class

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+	private String name;
+	private String email;
+	private String address;
+	private String phoneNumber;
+
+	private String birthday;
+	private double weight;
+	private double height;
+	private String bloodType;
+
+	Patient (String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+}
