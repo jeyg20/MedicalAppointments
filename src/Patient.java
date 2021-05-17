5d3cee0185bbc9f949e2526ba8a0ae3bebf80966
Jeison Guerrero <jei_son1516@hotmail.com>
2021-05-17 11:21:07 -0400

    getter and setters for patient class

--- src/Patient.java
+++ src/Patient.java
@@ -1,8 +1,4 @@
-public class Patient {
-	private String name;
-	private String email;
-	private String address;
-	private String phoneNumber;
+public class Patient extends User {
 
 	private String birthday;
 	private double weight;
@@ -14,4 +10,35 @@ public class Patient {
 		this.email = email;
 	}
 
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public String getBloodType() {
+		return bloodType;
+	}
+
+	public void setBloodType(String bloodType) {
+		this.bloodType = bloodType;
+	}
 }
